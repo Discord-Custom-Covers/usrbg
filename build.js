4d241d15cff67926b3d326bc7a67d547d57ff350
Delta-official <darkussdelta@gmail.com>
2022-03-03 00:00:37 +0000

    fix the build system and hopefully make it version agnostic

--- build.js
+++ build.js
@@ -17,7 +17,7 @@ async function compile() {
     const data = await read()
     writeFileSync("./dist/usrbg.json", JSON.stringify(data))
 
-    const createRule = (uids, rules) => `${uids.map(uid => `.root-3QyAh1[data-user-id="${uid}"],.userPopout-xaxa6l[data-user-id="${uid}"]`).join()}{${rules.join("")}}`
+    const createRule = (uids, rules) => `${uids.map(uid => `*[src*=\"${uid}\"],*[data-user-id\"${uid}\"]`).join()}{${rules.join("")}}`
 
     const backgrounds = new Map(Object.entries({ none: new Map, left: new Map, right: new Map }))
 
@@ -35,7 +35,7 @@ async function compile() {
             else return createRule(uids, [`--user-background:url("${img}");`, `--user-popout-position:${orientation}!important`])
         }).join("");
     }).join("");
-    return ".userPopout-xaxa6l{--user-popout-position:center}" + css
+    return "[class^=\"userPopout-\"{--user-popout-position:center}" + css
 }
 
 
